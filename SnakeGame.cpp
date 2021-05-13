008ca61f55f643f9c5eba1ca9ed450b0a1918701
kueyeon0429 <20203065@kookmin.ac.kr>
2021-05-13 21:53:14 +0900

    sujeong

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -5,23 +5,23 @@ int main()
   WINDOW *win1;
 
   initscr();
-  resize_term(100, 100);
+  resize_term(50, 50);
   start_color();
   init_pair(1, COLOR_WHITE, COLOR_YELLOW);
   init_pair(2, COLOR_WHITE, COLOR_GREEN);
 
 
   border('|', '|', '-', '-', '+', '+', '+', '+');
-  mvprintw(1, 1, "Presh any button >> SnakeGame!");
+  mvprintw(1, 1, "SnakeGame!");
   refresh();
   getch();
 
-  win1 = newwin(51, 51, 2, 1);
+  win1 = newwin(21, 21, 2, 1);
   wbkgd(win1, '0');
   wattron(win1, COLOR_PAIR(1));
-  mvwprintw(win1, 1, 1, "3");  // 지렁이
-  mvwprintw(win1, 1, 2, "4");  // 지렁이
-  mvwprintw(win1, 1, 3, "5");  // 지렁이
+  mvwprintw(win1, 1, 1, "3");  // 지렁이 head
+  mvwprintw(win1, 1, 2, "4");  // 지렁이 body
+  mvwprintw(win1, 1, 3, "4");  // 지렁이 body
   wattroff(win1, COLOR_PAIR(1));
 
   wattron(win1, COLOR_PAIR(2));
