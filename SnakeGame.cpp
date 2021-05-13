63ec35200600e96ad3ae11dfe46df4a3aa6ecbc0
kueyeon0429 <20203065@kookmin.ac.kr>
2021-05-13 23:18:32 +0900

    i don't  know...

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -1,27 +1,37 @@
+#include <clocale>
 #include <ncurses.h>
+#include <iostream>
 
 int main()
 {
-  WINDOW *win1;
-
+  setlocale(LC_ALL, "");
   initscr();
-  resize_term(50, 50);
   start_color();
-  init_pair(1, COLOR_WHITE, COLOR_YELLOW);
-  init_pair(2, COLOR_WHITE, COLOR_GREEN);
+  init_pair(1, COLOR_WHITE, COLOR_YELLOW);   // 팔레트1
+  init_pair(2, COLOR_WHITE, COLOR_GREEN);    // 팔레트2
 
-
-  border('|', '|', '-', '-', '+', '+', '+', '+');
+  //*******터미널*********
+  resize_term(24, 50);
+  border('|', '|', '-', '-', '+', '+', '+', '+');   // 테두리
   mvprintw(1, 1, "SnakeGame!");
-  refresh();
-  getch();
+  refresh();   // 화면 출력
+  noecho();   // 입력한 문자 가리기
+  getch();   // 문자 입력시 다음 이동
 
-  win1 = newwin(21, 21, 2, 1);
-  wbkgd(win1, '0');
+  //*******맵*********
+  WINDOW *win1;
+  win1 = newwin(21, 21, 2, 1);   // 맵 생성
+  wbkgd(win1, '0');   // 맵 배경
+
+  keypad(win1, TRUE);   // 특수 키 입력 가능
+  curs_set(0);   // 커서 가림
+
   wattron(win1, COLOR_PAIR(1));
   mvwprintw(win1, 1, 1, "3");  // 지렁이 head
   mvwprintw(win1, 1, 2, "4");  // 지렁이 body
   mvwprintw(win1, 1, 3, "4");  // 지렁이 body
+  //mvwprintw(win1, 1, 1, "\u2B1B");  // 지렁이 head 유니코드
+  //mvwprintw(win1, 1, 3, "\u2B1C");  // 지렁이 body 유니코드
   wattroff(win1, COLOR_PAIR(1));
 
   wattron(win1, COLOR_PAIR(2));
