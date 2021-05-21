d02312b5a27cd83e05aa5587619baf979b8eedfa
taekun20 <dbxorms2310@naver.com>
2021-05-22 05:35:18 +0900

    asdffgg

--- Snake.h
+++ Snake.h
@@ -4,17 +4,21 @@ typedef struct SNAKE {
   int length;    // snake 길이, 최소 3
   int head;       // head방향
   int ch = KEY_LEFT;    // 방향키의 방향
-  clock_t startTime;   // 시작 시간
   clock_t standTime;   // 기준 시간
   clock_t currTime;   // 현재 시간
+  bool fail;  // 게임지속
+  WINDOW *win;   // 윈도우
+  WINDOW *res;
   clock_t InsertGrowthItemTime;
   clock_t DeleteGrowthItemTime;
   clock_t InsertPoisonItemTime;
   clock_t DeletePoisonItemTime;
+  clock_t InsertGateTime;  // 게이트 생성 시간
+  clock_t DeleteGateTime;
   vector<vector<int>> vec;   //뱀 위치 정보
   vector<vector<int>> vecGrowthItem; //길어지는 아이템 모음
   vector<vector<int>> vecPoisonItem; //짧아지는 아이템 모음
-  bool fail;
-  WINDOW *res;
-  WINDOW *win;   // 윈도우
+  vector<int> vec1;  //  진입: 4방향, y, x
+  vector<int> vec2;  //  진출: 4방향, y, x
+  int gateCheck = 0; //임시변수
 } SNAKE;
